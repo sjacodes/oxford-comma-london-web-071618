d328f8f0896152ac30a0f3ba7a97f46b79afaacd
Sarah Jacob <smjacob91@gmail.com>
2018-07-05 13:26:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   array.join(",")
 end
 
-def oxford_comma(array1)
-  array1.join(" and ")
+def oxford_comma(array_1)
+  array_1.join(" and ")
 end
 
 def oxford_comma(3_element_array)
