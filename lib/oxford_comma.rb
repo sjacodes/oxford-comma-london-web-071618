d568b9ca1354571fe4a6d54abd1b40214867a9b4
Sarah Jacob <smjacob91@gmail.com>
2018-07-05 13:21:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,8 +8,7 @@ end
 
 def oxford_comma(array)
  new_array = array(-1).prepend("and")
- end
- new_array.join (" , ")
+ new_array.join (",")
 end
  
   it "adds commas plus a final 'and' when given a 3-element array" do
