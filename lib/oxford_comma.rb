20fb9d2b48e28aa5be57382eab835334ee00c6c6
Sarah Jacob <smjacob91@gmail.com>
2018-07-05 13:39:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,2 @@
 def oxford_comma(array)
-  if array.length == 1
-    return array
-  end
-end
-
-def oxford_comma(array2)
-   if array2.length == 2
-     array2.join(" and ")
-   return array2
- end
-end
-
-
+  if array.length 
