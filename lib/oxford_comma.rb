6a215cf8417808105a12bf1587a8d140ce8d31cc
Sarah Jacob <smjacob91@gmail.com>
2018-07-05 13:31:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array_1)
   array_1.join(" and ")
 end
 
-def oxford_comma(3_element_array
+def oxford_comma(3_element_array)
   if 3_element_array.length > 2
   puts 3_element_array[0..-2].join(',') + " and " + array[-1] 
   end
