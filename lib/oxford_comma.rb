16c3d12764afb619f4d1a3cb1139c41f907cd582
Sarah Jacob <smjacob91@gmail.com>
2018-07-05 13:36:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,13 @@ def oxford_comma(array)
 end
 
 def oxford_comma(array2)
+   if array.length == 2
   array2.join(" and ")
 end
 
+
+def oxford_comma(array3)
+  if array3.length == 3
+    puts array[0..-2].join(',') + " and " + array[-1] 
+  end
+end
