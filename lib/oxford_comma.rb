106d45d97fd8b22835bfeb23f1c4753d74111b6b
Sarah Jacob <smjacob91@gmail.com>
2018-07-05 13:43:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,8 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join (" and ")
-  elsif array.length >= 3
-    
+  else array.length >= 3
+     array[0..-2].join(",") + " and " + array[-1] 
+   end
+
     
