b7211527945ae1f8503036016ec94033f08ca1ec
Sarah Jacob <smjacob91@gmail.com>
2018-07-05 13:35:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,16 +4,7 @@ def oxford_comma(array)
   end
 end
 
-def oxford_comma(array)
-  array.join(",")
+def oxford_comma(array2)
+  array2.join(" and ")
 end
 
-def oxford_comma(array)
-  array.join(" and ")
-end
-
-def oxford_comma(array)
-  if array.length >= 3
-    puts array[0..-2].join(',') + " and " + array[-1] 
-  end
-end
